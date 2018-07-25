dbe296ab94440546e0619a11bee9ebd29513dc17
Mallory Frantz <mallory.rosenberger@gmail.com>
2018-07-25 23:22:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if(string.toLowerCase()) {
-    return "YES INDEED!"
-  } else if(string.toUpperCase()) {
     return "I can\'t hear you!"
+  } else if(string.toUpperCase()) {
+    return "YES INDEED!"
   } else if(string === "I love you, Grandma.") {
-    return "I love you, too."
+    return 
   }
 }
